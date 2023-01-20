06ad29b861ac74f37aa86837e74f0dc79dff467a
Jared Beard <32805348+jaredjbeard@users.noreply.github.com>
2023-01-20 13:05:18 -0500

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../../nestifydict/"))
+
+project = 'nestifydict'
+copyright = '2022, Jared J. Beard'
+author = 'Jared J. Beard'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage']
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+  html_theme = 'sphinx_rtd_theme'
+  html_static_path = ['_static']
